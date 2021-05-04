3448d8b10f92300e7f1a9bc0c0e8b82adf19d00e
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2021-05-04 14:41:20 +0800

    update for zelcore-io

--- src/networks.js
+++ src/networks.js
@@ -228,6 +228,26 @@ module.exports = {
     coin: coins.ZEC,
     hashFunctions: hashFunctions
   },
+  flux: {
+    messagePrefix: '\x18ZelCash Signed Message:\n',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x1cb8,
+    scriptHash: 0x1cbd,
+    wif: 0x80,
+    // This parameter was introduced in version 3 to allow soft forks, for version 1 and 2 transactions we add a
+    // dummy value.
+    consensusBranchId: {
+      1: 0x00,
+      2: 0x00,
+      3: 0x5ba81b19,
+      4: 0x76b809bb
+    },
+    coin: coins.ZEC,
+    hashFunctions: hashFunctions
+  },
   zero: {
     messagePrefix: '\x18Zero Signed Message:\n',
     bip32: {
