d01b27dbc7598da75ad03d6d6be36e8f21782ee2
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2021-08-19 21:23:26 +0200

    fix vtc

--- src/networks.js
+++ src/networks.js
@@ -528,6 +528,7 @@ module.exports = {
   },
   vertcoin: {
     messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bech32: 'vtc',
     bip32: {
       public: 0x0488b21e,
       private: 0x0488ade4
