c44be2953cd9fa5e21b2c6c980898debd94b4270
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2022-06-02 11:03:16 +0700

    zec nu5

--- src/networks.js
+++ src/networks.js
@@ -118,7 +118,7 @@ module.exports = {
       1: 0x00,
       2: 0x00,
       3: 0x5ba81b19,
-      4: 0xe9ff75a6
+      4: 0xc2d6d0b4
     },
     coin: coins.ZEC,
     hashFunctions: hashFunctions
@@ -136,7 +136,7 @@ module.exports = {
       1: 0x00,
       2: 0x00,
       3: 0x5ba81b19,
-      4: 0xe9ff75a6
+      4: 0xc2d6d0b4
     },
     coin: coins.ZEC,
     hashFunctions: hashFunctions
